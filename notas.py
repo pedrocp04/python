2bb66604ba0970efd5cf4cd7086bac76216fd024
Pedro Pigatto <pedropigatto54@gmail.com>
2021-04-01 21:23:29 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+nota_a= float(input("Digite a nota 1: ")
+nota_b= float(input("Digite a nota 2: ")
+	  
+media = (nota_a + nota_b)/2
 
-nota_a = 1
-nota_b = 2
-
-if (nota_a + nota_b) > 6:
+if media > 6:
 	print("Aprovado !")
 else:
 	print("Reprovado !")
